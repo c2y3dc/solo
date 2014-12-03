5f2cb48c09488995711b9397cc0cbc62847073b4
Ervin Chow <ervinchow@gmail.com>
2014-12-02 17:25:35 -0800

    implemented backwards and forwards and CSS/icons

--- reader.js
+++ reader.js
@@ -1,12 +1,21 @@
 angular.module('reader', [])
   .controller('readerCtrl', function($scope){
   	$scope.display = 0;
+  	$scope.counter = 0;
   	$scope.increment = function(){
-	  	$scope.str = document.getElementById("words").value;
 	  	// $scope.text = $scope.str.split(" ");
-	  	$scope.words_set();
+	  	document.body.style.backgroundColor= 'rgba(0,0,0,0.8)';
+	  	document.getElementById("words").style.backgroundColor="rgba(0,0,0,0)";
+	  	document.getElementById("buttons").style.backgroundColor="rgba(0,0,0,0)";
 
-  		setInterval(function(){
+  		$scope.str = document.getElementById("words").value;
+	  	$scope.words_set();
+	  	$scope.counter++;
+
+  		clearInterval($scope.refreshIntervalID);
+
+  		$scope.refreshIntervalID = setInterval(function(){
+  		// var refreshIntervalID = setInterval(fnc, ###)
         $scope.$apply(function() {
 					if($scope.display === $scope.text.length){
 						$scope.display = 0;
@@ -16,7 +25,29 @@ angular.module('reader', [])
 						$scope.word = "";
 						$scope.word = $scope.text[$scope.display];
         });
-    	}, 150);
+    	}, 150-$scope.counter*20);
+  	}
+  	$scope.prev = function(){
+  		$scope.str = document.getElementById("words").value;
+	  	$scope.words_set();
+  		if($scope.display === $scope.text.length){
+				$scope.display = $scope.text.length;
+				console.log($scope.display);
+			}
+				$scope.word = "";
+				$scope.word = $scope.text[$scope.display];
+				$scope.display--;	
+  	}
+  	$scope.next = function(){
+  		$scope.str = document.getElementById("words").value;
+		  $scope.words_set();
+
+  		if($scope.display === $scope.text.length){
+						$scope.display = 0;
+				}
+			$scope.word = "";
+			$scope.word = $scope.text[$scope.display];
+			$scope.display++;	
   	}
 
 		$scope.words_set = function(){
@@ -26,7 +57,7 @@ angular.module('reader', [])
 		.replace(/[ \t]{2,}/g, ' ')
 		.split(' ');
 		for (var j = 1; j < $scope.text.length; j++) {
-		$scope.text[j] = $scope.text[j].replace(/{linebreak}/g, '    ');
+		$scope.text[j] = $scope.text[j].replace(/{linebreak}/g, ' ');
 		}
 		}
 
